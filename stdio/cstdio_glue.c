b8c0b2a82a27f44e9f1ae51102f14e6fa4cddb9c
Bemly <bemly_@petalmail.com>
2025-04-30 04:12:28 +0800

    feat: covert ffi -> runtime fun

--- stdio/cstdio_glue.c
+++ stdio/cstdio_glue.c
@@ -8,78 +8,85 @@
 // 外部类型（extern type T）也被分配在堆上，
 // 但它们表示外部指针，因此 MoonBit 不会对它们执行任何引用计数操作。
 
+///////////////////////
+
+// 目前 runtime.c 一致的
+
+// moonbit_fopen_ffi
+// moonbit_fclose_ffi
+// moonbit_remove_file_ffi
+// moonbit_fflush_ffi
+// moonbit_ftell_ffi
+
+
+///////////////////////
+
+// 目前 runtime.c 已有的，但类型不同的
+
 // MOONBIT_FFI_EXPORT
-// void *fopen_ffi(moonbit_bytes_t filename, moonbit_bytes_t mode) {
-//     return fopen((const char *)filename, (const char *)mode);
+// int32_t fseek_ffi(void *stream, int64_t offset, int32_t whence) {
+//     return fseek((FILE *)stream, offset, whence);
 // }
 
-MOONBIT_FFI_EXPORT
-void *freopen_ffi(moonbit_bytes_t filename, moonbit_bytes_t mode, void *file) {
-    return freopen((const char *)filename, (const char *)mode, (FILE *)file);
-}
+// MOONBIT_FFI_EXPORT
+// uint64_t fread_ffi(void *ptr, uint64_t size, uint64_t nmemb, void *stream) {
+//     return fread(ptr, (size_t)size, (size_t)nmemb, (FILE *)stream);
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t fclose_ffi(void *stream) {
-    return fclose((FILE *)stream);
-}
+// MOONBIT_FFI_EXPORT
+// uint64_t fwrite_ffi(const void *ptr, uint64_t size, uint64_t nmemb, void *stream) {
+//     return fwrite(ptr, (size_t)size, (size_t)nmemb, (FILE *)stream);
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t feof_ffi(void *stream) {
-    return feof((FILE *)stream);
-}
 
-MOONBIT_FFI_EXPORT
-int32_t ferror_ffi(void *stream) {
-    return ferror((FILE *)stream);
-}
+///////////////////////
 
-MOONBIT_FFI_EXPORT
-int32_t fflush_ffi(void *stream) {
-    return fflush((FILE *)stream);
-}
+// MOONBIT_FFI_EXPORT
+// void *freopen_ffi(
+//     moonbit_bytes_t filename, 
+//     moonbit_bytes_t mode, 
+//     void *file) {
+//     return freopen(
+//         (const char *)filename, 
+//         (const char *)mode, 
+//         (FILE *)file);
+// }
 
-MOONBIT_FFI_EXPORT
-void clearerr_ffi(void *stream) {
-    clearerr((FILE *)stream); return;
-}
+// MOONBIT_FFI_EXPORT
+// int32_t feof_ffi(void *stream) {
+//     return feof((FILE *)stream);
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t fseek_ffi(void *stream, int64_t offset, int32_t whence) {
-    return fseek((FILE *)stream, offset, whence);
-}
+// MOONBIT_FFI_EXPORT
+// int32_t ferror_ffi(void *stream) {
+//     return ferror((FILE *)stream);
+// }
 
-MOONBIT_FFI_EXPORT
-int64_t ftell_ffi(void *stream) {
-    return ftell((FILE *)stream);
-}
 
-MOONBIT_FFI_EXPORT
-void rewind_ffi(void *stream) {
-    rewind((FILE *)stream); return;
-}
+// MOONBIT_FFI_EXPORT
+// void clearerr_ffi(void *stream) {
+//     clearerr((FILE *)stream); return;
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t fgetpos_ffi(void *stream, int64_t *pos) {
-    return fgetpos((FILE *)stream, (fpos_t *)pos);
-}
 
-MOONBIT_FFI_EXPORT
-int32_t fsetpos_ffi(void *stream, int64_t *pos) {
-    return fsetpos((FILE *)stream, (const fpos_t *)pos);
-}
 
-MOONBIT_FFI_EXPORT
-uint64_t fread_ffi(void *ptr, uint64_t size, uint64_t nmemb, void *stream) {
-    return fread(ptr, (size_t)size, (size_t)nmemb, (FILE *)stream);
-}
+// MOONBIT_FFI_EXPORT
+// void rewind_ffi(void *stream) {
+//     rewind((FILE *)stream); return;
+// }
 
-MOONBIT_FFI_EXPORT
-uint64_t fwrite_ffi(const void *ptr, uint64_t size, uint64_t nmemb, void *stream) {
-    return fwrite(ptr, (size_t)size, (size_t)nmemb, (FILE *)stream);
-}
+// MOONBIT_FFI_EXPORT
+// int32_t fgetpos_ffi(void *stream, int64_t *pos) {
+//     return fgetpos((FILE *)stream, (fpos_t *)pos);
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t ungetc_ffi(int32_t c, void *stream) {
-    return ungetc(c, (FILE *)stream);
-}
+// MOONBIT_FFI_EXPORT
+// int32_t fsetpos_ffi(void *stream, int64_t *pos) {
+//     return fsetpos((FILE *)stream, (const fpos_t *)pos);
+// }
+
+// MOONBIT_FFI_EXPORT
+// int32_t ungetc_ffi(int32_t c, void *stream) {
+//     return ungetc(c, (FILE *)stream);
+// }
 
